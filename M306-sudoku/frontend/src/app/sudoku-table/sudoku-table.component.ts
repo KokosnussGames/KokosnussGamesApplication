bbd54f2e422535d1358f20abf80887c3f988f9b7
CraftingDragon007 <info@craftingdragon.ch>
2026-05-07 11:23:12 +0200

    Migrated from jquery and axios to angular native methods like view child, events and http client, also prevent user from entering to invalid numbers/keys (client side validation)

--- M306-sudoku/frontend/src/app/sudoku-table/sudoku-table.component.ts
+++ M306-sudoku/frontend/src/app/sudoku-table/sudoku-table.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import $ from "jquery";
-// import * as axios from "axios";
-// const axios = require("axios").default;
-import axios from "axios";
+import { HttpClient } from "@angular/common/http";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-sudoku-table",
@@ -10,75 +7,68 @@ import axios from "axios";
   templateUrl: "./sudoku-table.component.html",
   styleUrls: ["./sudoku-table.component.css"]
 })
-export class SudokuTableComponent implements OnInit {
-  constructor() {}
+export class SudokuTableComponent {
+  @ViewChild("sudokuTable", { static: true }) sudokuTableRef!: ElementRef<HTMLTableElement>;
 
-  ngOnInit(): void {
-    $(document).ready(function() {
-      // Add event listenner on cell input, cell input will be send to the java backend for confirmation of input
-      $("td").keyup(function(event: any) {
-        let cell = event.currentTarget;
+  constructor(private readonly http: HttpClient) {}
 
-        // if the input is not a valid number (1-9) we clear the cell
-        var pattern = /^[1-9]+$/;
-        let currentValue = cell.innerText; //x
-        let isInputValid = pattern.test(currentValue);
-        if (!isInputValid) {
-          cell.innerText = "";
-          $(cell).removeClass("incorrect-input");
-          return false;
-        }
+  onTableKeyup(event: KeyboardEvent): void {
+    const target = event.target;
+    if (!(target instanceof HTMLTableCellElement) || !target.isContentEditable) {
+      return;
+    }
 
-        let cellIndex = cell.getAttribute("cell-index");
-        cellIndex = parseInt(cellIndex, 10);
-        let rowIndex = Math.floor(cellIndex / 9); //y
-        let cellIndexInRow = cellIndex - rowIndex * 9; //x
-        console.log(`current input on : 
-        - cellIndex : ${cellIndex}
-        - rowIndex : ${rowIndex} 
-        - cellIndexInRow : ${cellIndexInRow}
-        - currentValue : ${currentValue}`);
+    const currentValue = target.innerText.trim();
+    if (!/^[1-9]$/.test(currentValue)) {
+      target.innerText = "";
+      target.classList.remove("incorrect-input");
+      return;
+    }
 
-        //sudoku table representation
-        var listTD = $("td");
-        var sudokuTable = new Array(81);
-        $.each(listTD, function(index: number, cell: any) {
-          let cellIndexListTD = cell.getAttribute("cell-index");
-          let cellVal = cell.innerText;
-          if (cellVal == "" || cellIndexListTD == cellIndex) {
-            cellVal = 0;
-          } else {
-            cellVal = parseInt(cellVal);
-          }
-          sudokuTable[cellIndexListTD] = cellVal;
-        });
+    const indexAttribute = target.getAttribute("cell-index");
+    if (indexAttribute === null) {
+      return;
+    }
 
-        var jsonToSend = {
-          val: parseInt(currentValue),
-          index: cellIndex,
-          sudokuTable: sudokuTable
-        };
-        console.log(jsonToSend);
+    const cellIndex = Number.parseInt(indexAttribute, 10);
+    const sudokuTable = this.buildSudokuTable(cellIndex);
+    const payload = {
+      val: Number.parseInt(currentValue, 10),
+      index: cellIndex,
+      sudokuTable
+    };
 
-        // use axios module to send the json to the backend
-        axios
-          .post("http://localhost:8081/move", jsonToSend)
-          .then((response: any) => {
-            if (response.data == true) {
-              $(cell).removeClass("incorrect-input");
-              return true;
-            } else {
-              $(cell).addClass("incorrect-input");
-              return false;
-            }
-          })
-          .catch((error: any) => {
-            console.log(error);
-            return false;
-          });
-
-        return true;
-      }); // td keyup
+    this.http.post<boolean>("http://localhost:8081/move", payload).subscribe({
+      next: (isAccepted: boolean) => {
+        target.classList.toggle("incorrect-input", !isAccepted);
+      },
+      error: () => {
+        target.classList.add("incorrect-input");
+      }
     });
+  }
+
+  private buildSudokuTable(currentCellIndex: number): number[] {
+    const sudokuTable = new Array<number>(81).fill(0);
+    const cells = this.sudokuTableRef.nativeElement.querySelectorAll<HTMLTableCellElement>("td[cell-index]");
+
+    cells.forEach((cell: HTMLTableCellElement) => {
+      const indexAttribute = cell.getAttribute("cell-index");
+      if (indexAttribute === null) {
+        return;
+      }
+
+      const cellIndex = Number.parseInt(indexAttribute, 10);
+      const cellValueText = cell.innerText.trim();
+      if (cellValueText === "" || cellIndex === currentCellIndex) {
+        sudokuTable[cellIndex] = 0;
+        return;
+      }
+
+      const parsedValue = Number.parseInt(cellValueText, 10);
+      sudokuTable[cellIndex] = Number.isNaN(parsedValue) ? 0 : parsedValue;
+    });
+
+    return sudokuTable;
   }
 }
